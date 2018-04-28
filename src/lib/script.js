8ac6e14e40f62b2f119ca75591a40b2a23b819e0
Григорий <grisha15555@mail.ru>
2018-04-28 13:00:19 +0300

    It's work!

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,163 +1,177 @@
-/**
- * @param {org.truevote.setVote} tx
- * @transaction
- */
+  /**
+* @param {org.truevote.setVote} tx
+* @transaction
+*/
 async function onAcceptTrade(tx) {
 
-  // получаем регистр избирателей
-  const vr = await getParticipantRegistry('org.truevote.Voter');
-  // получаем регистр кандидатов
-  const cr = await getParticipantRegistry('org.truevote.Candidate');
-  // получаем регистр биллютеней
-  const br = await getAssetRegistry('org.truevote.Bulletin');
-  // кандидат
-  const candidate = await cr.get(tx.bulletin.candidate.getIdentifier());
+//
+const vr = await getParticipantRegistry('org.truevote.Voter');
+//
+const cr = await getParticipantRegistry('org.truevote.Candidate');
+//
+const br = await getAssetRegistry('org.truevote.Bulletin');
+const cor = await getParticipantRegistry('org.truevote.Counter');
+//
+const candidate = await cr.get(tx.bulletin.candidate.getIdentifier());
+const counter = await cor.get(tx.bulletin.counter.getIdentifier());
 
-  // если избиратель уже проголосовал, товыкидываем ошибку
-  if (!tx.voter.vote) {
-    throw new Error("Have not vote");
-  }
+// ,
+if (!tx.voter.vote) {
+throw new Error("Have not vote");
+}
 
-  // снимаём со счёта избирателя голос
-  tx.voter.vote = false;
-  // добавляем голос кандидату
-  candidate.votes += 1;
-  // заполняем биллютень
-  tx.bulletin.Party = candidate.Party;
-  // биллютень оффициально продан
-  tx.bulletin.movementStatus = 'IN_FIELD';
+//
+tx.voter.vote = false;
+//
+candidate.votes += 1;
+//
+tx.bulletin.Party = candidate.Party;
 
-  // обновляем избирателя
-  await vr.update(tx.voter);
-  // обновляем кандидата
-  await cr.update(candidate);
-  // обновляем биллютень
-  await br.update(tx.bulletin);
+//
+await vr.update(tx.voter);
+//
+await cr.update(candidate);
+//
+await br.update(tx.bulletin);
 
-  // создаём новый биллютень
-  const factory = getFactory();
-  var mas = tx.bulletin.getIdentifier().split('n');
-  var name = 'Bulletin' + mas[1] + '0';
-  var bulletinNew = factory.newResource('org.truevote', 'Bulletin', name);
+//
+const factory = getFactory();
+counter.count += 1;
+await cor.update(counter);
+var number = counter.count;
+var name = 'Bulletin' + number;
+var bulletinNew = factory.newResource('org.truevote', 'Bulletin', name);
 
-  // подготавливаем новый биллютень
-  bulletinNew.FIO = candidate.FIO;
-  bulletinNew.Party =  candidate.Party;
-  bulletinNew.bulletinId =  candidate.candidateId;
-  bulletinNew.movementStatus = 'IN_TRANSIT';
-  bulletinNew.candidate = candidate;
-  bulletinNew.bulletinId = name;
+//
+bulletinNew.FIO = candidate.FIO;
+bulletinNew.Party = candidate.Party;
+bulletinNew.bulletinId = candidate.candidateId;
+bulletinNew.movementStatus = 'IN_FIELD';
+bulletinNew.candidate = candidate;
+bulletinNew.bulletinId = name;
+bulletinNew.counter = counter;
 
-  // добавляем новый биллютень
-  await br.add(bulletinNew);
+//
+await br.add(bulletinNew);
 }
 
 //////////////////////////////////////////////////////////////////////
 /**
- * @param {org.truevote.setupDemo} setupDemo
- * @transaction
- */
+* @param {org.truevote.SetupDemo} setupDemo
+* @transaction
+*/
 async function setupDemo(setupDemo) {
-  // конструктор объектов
-  const factory = getFactory();
-  // наш namespace
-  const NS = 'org.truevote';
+//
+const factory = getFactory();
+// namespace
+const NS = 'org.truevote';
 
-  // получаем регистры
-  const vr = await getParticipantRegistry('org.truevote.Voter');
-  const cr = await getParticipantRegistry('org.truevote.Candidate');
-  const br = await getAssetRegistry('org.truevote.Bulletin');
+//
+const vr = await getParticipantRegistry('org.truevote.Voter');
+const cr = await getParticipantRegistry('org.truevote.Candidate');
+const br = await getAssetRegistry('org.truevote.Bulletin');
+const cor = await getParticipantRegistry('org.truevote.Counter');
 
-  // Создаём кандидатов
-  var candidates = [
-    factory.newResource(NS, 'Candidate', 'Candidate1@village.com'),
-    factory.newResource(NS, 'Candidate', 'Candidate2@village.com'),
-    factory.newResource(NS, 'Candidate', 'Candidate3@village.com'),
-  ];
+var counters = [
+  factory.newResource(NS, 'Counter', 'Counter1')
+];
 
-  // Создаём начальные биллютени
-  var bulletins = [
-    factory.newResource(NS, 'Bulletin', 'Bulletin1'),
-    factory.newResource(NS, 'Bulletin', 'Bulletin2'),
-    factory.newResource(NS, 'Bulletin', 'Bulletin3'),
-  ];
+//
+var candidates = [
+factory.newResource(NS, 'Candidate', 'Candidate1@village.com'),
+factory.newResource(NS, 'Candidate', 'Candidate2@village.com'),
+factory.newResource(NS, 'Candidate', 'Candidate3@village.com'),
+];
 
-  // create animals
-  var voters = [
-    factory.newResource(NS, 'Voter', 'Voter1'),
-    factory.newResource(NS, 'Voter', 'Voter2'),
-    factory.newResource(NS, 'Voter', 'Voter3'),
-    factory.newResource(NS, 'Voter', 'Voter4'),
-    factory.newResource(NS, 'Voter', 'Voter5'),
-    factory.newResource(NS, 'Voter', 'Voter6'),
-    factory.newResource(NS, 'Voter', 'Voter7'),
-    factory.newResource(NS, 'Voter', 'Voter8'),
-    factory.newResource(NS, 'Voter', 'Voter9'),
-  ];
+//
+var bulletins = [
+factory.newResource(NS, 'Bulletin', 'Bulletin1'),
+factory.newResource(NS, 'Bulletin', 'Bulletin2'),
+factory.newResource(NS, 'Bulletin', 'Bulletin3'),
+];
 
-  // заполняем кандидатов
-  candidates[0].FIO = 'Ivanov';
-  candidates[0].Party = 'Left';
-  candidates[0].votes = 0;
+// create animals
+var voters = [
+factory.newResource(NS, 'Voter', 'Voter1'),
+factory.newResource(NS, 'Voter', 'Voter2'),
+factory.newResource(NS, 'Voter', 'Voter3'),
+factory.newResource(NS, 'Voter', 'Voter4'),
+factory.newResource(NS, 'Voter', 'Voter5'),
+factory.newResource(NS, 'Voter', 'Voter6'),
+factory.newResource(NS, 'Voter', 'Voter7'),
+factory.newResource(NS, 'Voter', 'Voter8'),
+factory.newResource(NS, 'Voter', 'Voter9'),
+];
 
-  candidates[1].FIO = 'Petrov';
-  candidates[1].Party = 'Center';
-  candidates[1].votes = 0;
+counters[0].count = 3;
+await cor.addAll(counters);
 
-  candidates[2].FIO = 'Sobolev';
-  candidates[2].Party = 'Right';
-  candidates[2].votes = 0;
+//
+candidates[0].FIO = 'Ivanov';
+candidates[0].Party = 'Left';
+candidates[0].votes = 0;
 
-  // добавляем кандидатов в регистр
-  await cr.addAll(candidates);
+candidates[1].FIO = 'Petrov';
+candidates[1].Party = 'Center';
+candidates[1].votes = 0;
 
-  // заполняем биллютени
-  bulletins[0].FIO = 'Ivanov';
-  bulletins[0].Party = 'Left';
-  bulletins[0].movementStatus = 'IN_TRANSIT';
-  bulletins[0].candidate = factory.newRelationship(NS, 'Candidate', candidates[0].candidateId);
+candidates[2].FIO = 'Sobolev';
+candidates[2].Party = 'Right';
+candidates[2].votes = 0;
 
-  bulletins[1].FIO = 'Petrov';
-  bulletins[1].Party = 'Center';
-  bulletins[1].movementStatus = 'IN_TRANSIT';
-  bulletins[1].candidate = factory.newRelationship(NS, 'Candidate', candidates[1].candidateId);
+//
+await cr.addAll(candidates);
 
-  bulletins[2].FIO = 'Sobolev';
-  bulletins[2].Party = 'Right';
-  bulletins[2].movementStatus = 'IN_TRANSIT';
-  bulletins[2].candidate = factory.newRelationship(NS, 'Candidate', candidates[2].candidateId);
+//
+bulletins[0].FIO = 'Ivanov';
+bulletins[0].Party = 'Left';
+bulletins[0].movementStatus = 'IN_TRANSIT';
+bulletins[0].candidate = factory.newRelationship(NS, 'Candidate', candidates[0].candidateId);
+bulletins[0].counter = factory.newRelationship(NS, 'Counter', counters[0].counterId);
 
-  // добавляем бюллитени в регистр
-  await br.addAll(bulletins);
+bulletins[1].FIO = 'Petrov';
+bulletins[1].Party = 'Center';
+bulletins[1].movementStatus = 'IN_TRANSIT';
+bulletins[1].candidate = factory.newRelationship(NS, 'Candidate',candidates[1].candidateId);
+bulletins[1].counter = factory.newRelationship(NS, 'Counter', counters[0].counterId);
 
-  // заполняем избирателей
-  voters[0].FIO = 'Molly';
-  voters[0].vote = true;
+bulletins[2].FIO = 'Sobolev';
+bulletins[2].Party = 'Right';
+bulletins[2].movementStatus = 'IN_TRANSIT';
+bulletins[2].candidate = factory.newRelationship(NS, 'Candidate', candidates[2].candidateId);
+bulletins[2].counter = factory.newRelationship(NS, 'Counter', counters[0].counterId);
 
-  voters[1].FIO = 'Polly';
-  voters[1].vote = true;
+//
+await br.addAll(bulletins);
 
-  voters[2].FIO = 'Dolly';
-  voters[2].vote = true;
+//
+voters[0].FIO = 'Molly';
+voters[0].vote = true;
 
-  voters[3].FIO = 'Kolly';
-  voters[3].vote = true;
+voters[1].FIO = 'Polly';
+voters[1].vote = true;
 
-  voters[4].FIO = 'Rolly';
-  voters[4].vote = true;
+voters[2].FIO = 'Dolly';
+voters[2].vote = true;
 
-  voters[5].FIO = 'Tolly';
-  voters[5].vote = true;
+voters[3].FIO = 'Kolly';
+voters[3].vote = true;
 
-  voters[6].FIO = 'Ivanov';
-  voters[6].vote = true;
+voters[4].FIO = 'Rolly';
+voters[4].vote = true;
 
-  voters[7].FIO = 'Petrov';
-  voters[7].vote = true;
+voters[5].FIO = 'Tolly';
+voters[5].vote = true;
 
-  voters[8].FIO = 'Sobolev';
-  voters[8].vote = true;
+voters[6].FIO = 'Ivanov';
+voters[6].vote = true;
 
-  // добавляем кандидатов в регистр
-  await vr.addAll(voters);
+voters[7].FIO = 'Petrov';
+voters[7].vote = true;
+
+voters[8].FIO = 'Sobolev';
+voters[8].vote = true;
+
+//
+await vr.addAll(voters);
 }
